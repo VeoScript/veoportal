882ae038097016cdd982c7a5dd976b7d3b781160
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2023-05-15 13:28:44 +0800

    [feat] - add tech stacks display

--- tailwind.config.js
+++ tailwind.config.js
@@ -30,6 +30,10 @@ module.exports = {
           '100%': { transform: 'translateY(-100%)' },
         },
       },
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
     },
   },
   plugins: [],
