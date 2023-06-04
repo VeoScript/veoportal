74bd6b91c1d5b565c8ad15e602d3aa01a1378cd3
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2023-06-04 14:35:45 +0800

    feat: update home page layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,7 +15,7 @@ const Home = () => {
       </Head>
       <MainTemplate>
         <div className="flex flex-col items-center justify-center w-full h-screen">
-          <div className="flex flex-col items-center w-full max-w-full space-y-10">
+          <div className="flex flex-col items-center w-full max-w-full mt-10 px-5 md:px-0 space-y-10">
             <div
               className="flex items-center justify-center w-full"
               data-aos="zoom-in"
@@ -49,17 +49,17 @@ const Home = () => {
             </div>
           </div>
         </div>
-        <div className="flex flex-col items-center justify-center w-full h-screen border-t border-neutral-800 bg-black">
+        <div className="flex flex-col items-center justify-start md:justify-center w-full h-screen border-t border-neutral-800 bg-black">
           <div
-            className="flex flex-row items-start justify-center w-full max-w-5xl space-x-10"
-            data-aos="fade-up"
+            className="flex flex-col md:flex-row items-start justify-center w-full max-w-full md:max-w-5xl space-x-0 md:space-x-10"
+            data-aos="fade-left"
             data-aos-delay="200"
           >
-            <div className="flex w-full max-w-[20rem]">
+            <div className="flex w-full max-w-full md:max-w-[20rem]">
               <Image
                 src="/images/jeromevillaruel.jpg"
                 blurDataURL="/images/jeromevillaruel.jpg"
-                className="w-[20rem] h-[20rem] object-cover rounded-ss-[5rem] rounded-ee-[5rem] bg-white"
+                className="w-full md:w-[20rem] h-[20rem] object-cover rounded-none md:rounded-ss-[5rem] md:rounded-ee-[5rem] bg-white"
                 alt="veoscript"
                 placeholder="blur"
                 width={500}
@@ -67,7 +67,7 @@ const Home = () => {
                 quality={100}
               />
             </div>
-            <div className="flex flex-col items-start w-full py-3 space-y-10">
+            <div className="flex flex-col items-start w-full px-5 md:px-0 py-5 md:py-3 space-y-10">
               <div className="flex flex-col w-full">
                 <Text weight='bold' size='2xl'>Jerome Villaruel</Text>
                 <Text weight='light' size='base'>Software Developer</Text>
@@ -99,13 +99,13 @@ const Home = () => {
             <div className="flex flex-col items-center w-full">
               <Text font='rock_salt' color='pink' align='center' weight='bold' size='5xl'>Skills and Services</Text>
             </div>
-            <div className="flex flex-row items-start justify-between w-full space-x-5">
+            <div className="flex flex-col md:flex-row items-start justify-between w-full space-x-0 md:space-x-5 space-y-5 md:space-y-0">
               <div
                 className="relative flex flex-col items-center w-full space-y-3"
                 data-aos="fade-right"
                 data-aos-delay="200"
               >
-                <svg className="w-20 h-2w-20 fill-current text-accent-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                <svg className="w-16 md:w-20 h-16 md:h-20 fill-current text-accent-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                   <path d="M22 17v-11.8c0-.663-.537-1.2-1.2-1.2h-17.6c-.663 0-1.2.537-1.2 1.2v11.8h20zm-18-11h16v9h-16v-9zm20 12v.8c0 .663-.537 1.2-1.2 1.2h-21.6c-.663 0-1.2-.537-1.2-1.2v-.8h10c0 .276.224.5.5.5h3c.276 0 .5-.224.5-.5h10z"/>
                 </svg>
                 <Text align='center' weight='bold' size='2xl'>Web Development</Text>
@@ -116,7 +116,7 @@ const Home = () => {
                 data-aos="fade-up"  
                 data-aos-delay="400"
               >
-                <svg className="w-20 h-2w-20 fill-current text-accent-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                <svg className="w-16 md:w-20 h-16 md:h-20 fill-current text-accent-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                   <path d="M24 10.935v2.131l-8 3.947v-2.23l5.64-2.783-5.64-2.79v-2.223l8 3.948zm-16 3.848l-5.64-2.783 5.64-2.79v-2.223l-8 3.948v2.131l8 3.947v-2.23zm7.047-10.783h-2.078l-4.011 16h2.073l4.016-16z"/>
                 </svg>
                 <Text align='center' weight='bold' size='2xl'>Software Development</Text>
@@ -127,7 +127,7 @@ const Home = () => {
                 data-aos="fade-left"  
                 data-aos-delay="600"
               >
-                <svg className="w-20 h-2w-20 fill-current text-accent-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                <svg className="w-16 md:w-20 h-16 md:h-20 fill-current text-accent-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                   <path d="M5 9c1.654 0 3 1.346 3 3s-1.346 3-3 3-3-1.346-3-3 1.346-3 3-3zm0-2c-2.762 0-5 2.239-5 5s2.238 5 5 5 5-2.239 5-5-2.238-5-5-5zm15 9c-1.165 0-2.204.506-2.935 1.301l-5.488-2.927c-.23.636-.549 1.229-.944 1.764l5.488 2.927c-.072.301-.121.611-.121.935 0 2.209 1.791 4 4 4s4-1.791 4-4-1.791-4-4-4zm0 6c-1.103 0-2-.897-2-2s.897-2 2-2 2 .897 2 2-.897 2-2 2zm0-22c-2.209 0-4 1.791-4 4 0 .324.049.634.121.935l-5.488 2.927c.395.536.713 1.128.944 1.764l5.488-2.927c.731.795 1.77 1.301 2.935 1.301 2.209 0 4-1.791 4-4s-1.791-4-4-4zm0 6c-1.103 0-2-.897-2-2s.897-2 2-2 2 .897 2 2-.897 2-2 2z"/>
                 </svg>
                 <Text align='center' weight='bold' size='2xl'>I.T. Networking</Text>
@@ -208,8 +208,8 @@ const Home = () => {
             </div>
           </div>
         </div>
-        <div className="flex flex-col items-center justify-center w-full h-screen px-5 border-t border-neutral-800 bg-accent-1">
-          <div className="flex flex-col items-center w-full max-w-full space-y-24">
+        <div className="flex flex-col items-center justify-center w-full h-full md:h-screen px-5 border-t border-neutral-800 bg-accent-1">
+          <div className="flex flex-col items-center w-full max-w-full py-10 md:py-0 space-y-10 md:space-y-24">
             <div className="flex flex-col items-center w-full">
               <Text font='rock_salt' color='pink' align='center' weight='bold' size='5xl'>Tech Stacks</Text>
             </div>
@@ -218,11 +218,11 @@ const Home = () => {
               data-aos="zoom-in-up"
               data-aos-delay="200"
             >
-              <div className="grid grid-cols-3 gap-10">
+              <div className="grid grid-cols-2 md:grid-cols-3 gap-10">
                 <Link href="https://react.dev/" target="_blank" className="w-full transition ease-in-out duration-200 transform hover:scale-90">
                   <Image
                     src="/images/techstacks/reactjs.png"
-                    className="w-[8rem] h-[8rem] object-cover bg-transparent"
+                    className="w-[6rem] h-[6rem] object-cover bg-transparent"
                     alt="ReactJS"
                     width={500}
                     height={500}
@@ -232,7 +232,7 @@ const Home = () => {
                 <Link href="https://nextjs.org/" target="_blank" className="w-full transition ease-in-out duration-200 transform hover:scale-90">
                   <Image
                     src="/images/techstacks/nextjs.png"
-                    className="w-[8rem] h-[8rem] object-cover bg-transparent"
+                    className="w-[6rem] h-[6rem] object-cover bg-transparent"
                     alt="NextJS"
                     width={500}
                     height={500}
@@ -242,19 +242,18 @@ const Home = () => {
                 <Link href="https://nestjs.com/" target="_blank" className="w-full transition ease-in-out duration-200 transform hover:scale-90">
                   <Image
                     src="/images/techstacks/nestjs.png"
-                    className="w-[8rem] h-[8rem] object-cover bg-transparent"
+                    className="w-[6rem] h-[6rem] object-cover bg-transparent"
                     alt="NestJS"
                     width={500}
                     height={500}
                     quality={100}
                   />
                 </Link>
-              </div>
-              <div className='grid grid-cols-4 gap-10'>
+                
                 <Link href="https://tanstack.com/query/v3/" target="_blank" className="w-full transition ease-in-out duration-200 transform hover:scale-90">
                   <Image
                     src="/images/techstacks/reactquery.png"
-                    className="w-[8rem] h-[8rem] object-cover bg-transparent"
+                    className="w-[6rem] h-[6rem] object-cover bg-transparent"
                     alt="React Query"
                     width={500}
                     height={500}
@@ -264,7 +263,7 @@ const Home = () => {
                 <Link href="https://tailwindcss.com/" target="_blank" className="w-full transition ease-in-out duration-200 transform hover:scale-90">
                   <Image
                     src="/images/techstacks/tailwindcss.png"
-                    className="w-[8rem] h-[8rem] object-cover bg-transparent"
+                    className="w-[6rem] h-[6rem] object-cover bg-transparent"
                     alt="TailwindCSS"
                     width={500}
                     height={500}
@@ -274,7 +273,27 @@ const Home = () => {
                 <Link href="https://tauri.app/" target="_blank" className="w-full transition ease-in-out duration-200 transform hover:scale-90">
                   <Image
                     src="/images/techstacks/tauri.png"
-                    className="w-[8rem] h-[8rem] object-cover bg-transparent"
+                    className="w-[6rem] h-[6rem] object-cover bg-transparent"
+                    alt="Tauri"
+                    width={500}
+                    height={500}
+                    quality={100}
+                  />
+                </Link>
+                <Link href="https://ionicframework.com/" target="_blank" className="w-full transition ease-in-out duration-200 transform hover:scale-90">
+                  <Image
+                    src="/images/techstacks/ionic.png"
+                    className="w-[6rem] h-[6rem] object-cover bg-transparent"
+                    alt="Tauri"
+                    width={500}
+                    height={500}
+                    quality={100}
+                  />
+                </Link>
+                <Link href="https://graphql.org/" target="_blank" className="w-full transition ease-in-out duration-200 transform hover:scale-90">
+                  <Image
+                    src="/images/techstacks/graphql.png"
+                    className="w-[6rem] h-[6rem] object-cover bg-transparent"
                     alt="Tauri"
                     width={500}
                     height={500}
@@ -284,7 +303,7 @@ const Home = () => {
                 <Link href="https://www.prisma.io/" target="_blank" className="w-full transition ease-in-out duration-200 transform hover:scale-90">
                   <Image
                     src="/images/techstacks/prisma.png"
-                    className="w-[8rem] h-[8rem] object-cover bg-transparent"
+                    className="w-[6rem] h-[6rem] object-cover bg-transparent"
                     alt="Prisma"
                     width={500}
                     height={500}
@@ -292,6 +311,8 @@ const Home = () => {
                   />
                 </Link>
               </div>
+              {/* <div className='grid grid-cols-2 md:grid-cols-4 gap-10'>
+              </div> */}
             </div>
           </div>
         </div>
