9626d8010d7202cc990468e683658bc1ef7eb39f
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2023-06-27 17:30:34 +0800

    feat: add admin panel link in menu component

--- src/components/organisms/Menu.tsx
+++ src/components/organisms/Menu.tsx
@@ -6,7 +6,7 @@ const Menu = () => {
   const [isCollapse, setIsCollapse] = React.useState<boolean>(false)
 
   return (
-    <div className={`fixed top-1/2 right-5 md:right-10 z-20 animate-pulse ${isCollapse ? 'h-[15rem] -mt-20 hover:scale-100' : 'h-auto hover:scale-95'} transition ease-in-out duration-200 transform flex flex-col items-center w-auto space-y-5 rounded-md p-3 backdrop-blur-xl bg-white bg-opacity-10`}>
+    <div className={`fixed top-1/2 right-5 md:right-10 z-20 animate-pulse flex flex-col items-center justify-center ${isCollapse ? 'h-auto -mt-24 hover:scale-100' : 'h-auto hover:scale-95'} transition ease-in-out duration-200 transform flex flex-col items-center w-auto space-y-5 rounded-md p-3 backdrop-blur-xl bg-white bg-opacity-10`}>
       <button
         className="outline-none"
         onClick={() => isCollapse ? setIsCollapse(false) : setIsCollapse(true)}
@@ -50,6 +50,17 @@ const Menu = () => {
           >
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 md:w-10 h-8 md:h-10 text-white transition ease-in-out duration-200 transform hover:scale-90">
               <path strokeLinecap="round" strokeLinejoin="round" d="M12.75 3.03v.568c0 .334.148.65.405.864l1.068.89c.442.369.535 1.01.216 1.49l-.51.766a2.25 2.25 0 01-1.161.886l-.143.048a1.107 1.107 0 00-.57 1.664c.369.555.169 1.307-.427 1.605L9 13.125l.423 1.059a.956.956 0 01-1.652.928l-.679-.906a1.125 1.125 0 00-1.906.172L4.5 15.75l-.612.153M12.75 3.031a9 9 0 00-8.862 12.872M12.75 3.031a9 9 0 016.69 14.036m0 0l-.177-.529A2.25 2.25 0 0017.128 15H16.5l-.324-.324a1.453 1.453 0 00-2.328.377l-.036.073a1.586 1.586 0 01-.982.816l-.99.282c-.55.157-.894.702-.8 1.267l.073.438c.08.474.49.821.97.821.846 0 1.598.542 1.865 1.345l.215.643m5.276-3.67a9.012 9.012 0 01-5.276 3.67m0 0a9 9 0 01-10.275-4.835M15.75 9c0 .896-.393 1.7-1.016 2.25" />
+            </svg>
+          </Link>
+          <Link
+            title="Admin Panel"
+            className={`${isCollapse ? 'block' : 'hide'}`}
+            href="https://veoportal-admin.vercel.app/"
+            target="_blank"
+            onClick={() => setIsCollapse(false)}
+          >
+            <svg className="w-8 md:w-10 h-8 md:h-10 text-white transition ease-in-out duration-200 transform hover:scale-90" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M7.864 4.243A7.5 7.5 0 0119.5 10.5c0 2.92-.556 5.709-1.568 8.268M5.742 6.364A7.465 7.465 0 004.5 10.5a7.464 7.464 0 01-1.15 3.993m1.989 3.559A11.209 11.209 0 008.25 10.5a3.75 3.75 0 117.5 0c0 .527-.021 1.049-.064 1.565M12 10.5a14.94 14.94 0 01-3.6 9.75m6.633-4.596a18.666 18.666 0 01-2.485 5.33"></path>
             </svg>
           </Link>
         </>
