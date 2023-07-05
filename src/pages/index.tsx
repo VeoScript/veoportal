283fe822f5075a21914eb5df62c82ae8f26697eb
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2023-07-05 22:22:28 +0800

    style: adjust breakpoints (#18)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -69,7 +69,7 @@ const Home = () => {
           </div>
         </div>
         <div className="flex flex-col items-center justify-start md:justify-center w-full h-full md:h-screen border-t border-neutral-800 bg-black">
-          <div className="flex flex-col md:flex-row items-center justify-start md:justify-center w-full max-w-full md:max-w-5xl h-full space-x-0 md:space-x-10">
+          <div className="flex flex-col md:flex-row items-center justify-start md:justify-center w-full large-break-point:max-w-5xl extra-large-break-point:max-w-7xl h-full space-x-0 md:space-x-10">
             <div
               className="flex w-full max-w-full md:max-w-[20rem]"
               data-aos="fade-right"
@@ -94,7 +94,7 @@ const Home = () => {
                 <Text weight='light' size='base'>Software Developer</Text>
               </div>
               <div className="flex flex-col w-full">
-                <Text weight='light'>
+                <Text weight='light' align='justify'>
                   {`👋 Hello, I'm Jerome Villaruel officially known as Veoscript, a software developer based in Philippines specializing in React, NextJS, NestJS, RestAPI, GraphQL, Prisma, Supabase, and PlanetScale. I enjoy building dynamic web applications and leveraging these technologies to create robust and scalable solutions. Excited to collaborate on innovative projects and contribute to the world of software development.`}
                 </Text>
               </div>
@@ -116,11 +116,11 @@ const Home = () => {
           </div>
         </div>
         <div className="flex flex-col items-center justify-center w-full h-full md:h-screen px-5 border-t border-neutral-800 bg-accent-1">
-          <div className="flex flex-col items-center w-full max-w-full md:max-w-7xl py-10 md:py-0 space-y-10 md:space-y-24">
+          <div className="flex flex-col items-center w-full large-break-point:max-w-5xl extra-large-break-point:max-w-7xl py-10 md:py-0 space-y-10 md:space-y-24">
             <div className="flex flex-col items-center w-full">
               <Text font='rock_salt' color='pink' align='center' weight='bold' size='5xl'>Skills and Services</Text>
             </div>
-            <div className="flex flex-col md:flex-row items-start justify-between w-full space-x-0 md:space-x-5 space-y-5 md:space-y-0">
+            <div className="flex flex-col md:flex-row items-start justify-between w-full space-x-0 md:space-x-10 space-y-5 md:space-y-0">
               <div
                 className="relative flex flex-col items-center w-full space-y-3"
                 data-aos="fade-right"
@@ -130,7 +130,7 @@ const Home = () => {
                   <path d="M22 17v-11.8c0-.663-.537-1.2-1.2-1.2h-17.6c-.663 0-1.2.537-1.2 1.2v11.8h20zm-18-11h16v9h-16v-9zm20 12v.8c0 .663-.537 1.2-1.2 1.2h-21.6c-.663 0-1.2-.537-1.2-1.2v-.8h10c0 .276.224.5.5.5h3c.276 0 .5-.224.5-.5h10z"/>
                 </svg>
                 <Text align='center' weight='bold' size='2xl'>Web Development</Text>
-                <Text align='center' weight='light' size='sm'>Consider as a full-stack developer using Git, NextJS, NuxtJS, NestJS, Prisma, PostgreSQL, MySQL, MSSQL, PlanetScale, GraphQL and REST-API.</Text>
+                <Text align='left' weight='light' size='sm'>Consider as a full-stack developer using Git, NextJS, NuxtJS, NestJS, Prisma, PostgreSQL, MySQL, MSSQL, PlanetScale, GraphQL and REST-API.</Text>
               </div>
               <div
                 className="flex flex-col items-center w-full space-y-3"
@@ -141,7 +141,7 @@ const Home = () => {
                   <path d="M24 10.935v2.131l-8 3.947v-2.23l5.64-2.783-5.64-2.79v-2.223l8 3.948zm-16 3.848l-5.64-2.783 5.64-2.79v-2.223l-8 3.948v2.131l8 3.947v-2.23zm7.047-10.783h-2.078l-4.011 16h2.073l4.016-16z"/>
                 </svg>
                 <Text align='center' weight='bold' size='2xl'>Software Development</Text>
-                <Text align='center' weight='light' size='sm'>Can create desktop applications using Tauri, Electron, and C# WFA. Mobile applications both iOS & Android using React Native and Ionic.</Text>
+                <Text align='left' weight='light' size='sm'>Can create desktop applications using Tauri, Electron, and C# WFA. Mobile applications both iOS & Android using React Native and Ionic.</Text>
               </div>
               <div
                 className="flex flex-col items-center w-full space-y-3"
@@ -152,17 +152,17 @@ const Home = () => {
                   <path d="M5 9c1.654 0 3 1.346 3 3s-1.346 3-3 3-3-1.346-3-3 1.346-3 3-3zm0-2c-2.762 0-5 2.239-5 5s2.238 5 5 5 5-2.239 5-5-2.238-5-5-5zm15 9c-1.165 0-2.204.506-2.935 1.301l-5.488-2.927c-.23.636-.549 1.229-.944 1.764l5.488 2.927c-.072.301-.121.611-.121.935 0 2.209 1.791 4 4 4s4-1.791 4-4-1.791-4-4-4zm0 6c-1.103 0-2-.897-2-2s.897-2 2-2 2 .897 2 2-.897 2-2 2zm0-22c-2.209 0-4 1.791-4 4 0 .324.049.634.121.935l-5.488 2.927c.395.536.713 1.128.944 1.764l5.488-2.927c.731.795 1.77 1.301 2.935 1.301 2.209 0 4-1.791 4-4s-1.791-4-4-4zm0 6c-1.103 0-2-.897-2-2s.897-2 2-2 2 .897 2 2-.897 2-2 2z"/>
                 </svg>
                 <Text align='center' weight='bold' size='2xl'>I.T. Networking</Text>
-                <Text align='center' weight='light' size='sm'>Handle network topology, network security, microservices, database administrator and local area network.</Text>
+                <Text align='left' weight='light' size='sm'>Handle network topology, network security, microservices, database administrator and local area network.</Text>
               </div>
             </div>
           </div>
         </div>
         <div className="flex flex-col items-center justify-center w-full h-screen px-5 border-t border-neutral-800 bg-accent-2">
-          <div className="flex flex-col items-center w-full max-w-full space-y-24">
+          <div className="flex flex-col items-center w-full large-break-point:max-w-5xl extra-large-break-point:max-w-7xl space-y-24">
             <div className="flex flex-col items-center w-full">
               <Text font='rock_salt' color='pink' align='center' weight='bold' size='5xl'>Experiences</Text>
             </div>
-            <div className="flex flex-col items-center w-full max-w-2xl space-y-5">
+            <div className="flex flex-col items-center w-full max-w-full space-y-5">
               <div
                 className="flex flex-row items-center w-full p-3 space-x-5 overflow-hidden rounded-xl border border-neutral-800"
                 data-aos="fade-left"
@@ -230,7 +230,7 @@ const Home = () => {
           </div>
         </div>
         <div className="flex flex-col items-center justify-center w-full h-full md:h-screen px-5 border-t border-neutral-800 bg-accent-1">
-          <div className="flex flex-col items-center w-full max-w-full py-10 md:py-0 space-y-10 md:space-y-24">
+          <div className="flex flex-col items-center w-full large-break-point:max-w-5xl extra-large-break-point:max-w-7xl py-10 md:py-0 space-y-10 md:space-y-24">
             <div className="flex flex-col items-center w-full">
               <Text font='rock_salt' color='pink' align='center' weight='bold' size='5xl'>Tech Stacks</Text>
             </div>
@@ -239,7 +239,7 @@ const Home = () => {
               data-aos="zoom-in-up"
               data-aos-delay="200"
             >
-              <div className="flex flex-wrap items-center justify-center w-full max-w-2xl gap-10">
+              <div className="flex flex-wrap items-center justify-center w-full max-w-full gap-10">
                 <Link href="https://react.dev/" target="_blank" className="transition ease-in-out duration-200 transform hover:scale-110">
                   <Image
                     src="/images/techstacks/reactjs.png"
