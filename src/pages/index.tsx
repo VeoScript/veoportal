c0fda13c80900229b263f4e7f1fdde01790bd8b9
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2023-09-15 11:07:07 +0800

    feat: update experiences UI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,8 @@ import MainTemplate from '~/components/templates/MainTemplate'
 import { Facebook, Twitter, Github, LinkedIn } from '~/utils/Icons'
 import { Text } from '~/components/atoms/Text'
 import { Button } from '~/components/atoms/Button'
+import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component'
+import 'react-vertical-timeline-component/style.min.css'
 
 import { IExperience, ITechStacks } from '~/shared/interfaces'
 import { experiences } from '../shared/mocks/experiences'
@@ -177,40 +179,43 @@ const Home = () => {
             </div>
           </div>
         </div>
-        <div className="flex flex-col items-center justify-center w-full h-screen px-5 border-t border-neutral-800 bg-accent-2">
+        <div className="flex flex-col items-center justify-center w-full h-full px-5 py-[5rem] border-t border-neutral-800 bg-accent-2">
           <div className="flex flex-col items-center w-full large-break-point:max-w-5xl extra-large-break-point:max-w-7xl space-y-24">
             <div className="flex flex-col items-center w-full">
               <Text font='rock_salt' color='pink' align='center' weight='bold' size='5xl'>Experiences</Text>
             </div>
             <div className="flex flex-col items-center w-full max-w-full space-y-5">
-              {experiences.map((experience: IExperience, index: number) => (
-                <div
-                  key={index}
-                  className="flex flex-col md:flex-row items-start md:items-center justify-between w-full p-3 space-y-3 md:space-y-0 overflow-hidden rounded-xl border border-neutral-800"
-                  data-aos="fade-left"
-                  data-aos-delay="200"
-                >
-                  <div className="flex flex-row items-center w-auto space-x-5">
-                    <Image
+              <VerticalTimeline>
+                {experiences.map((experience: IExperience, index: number) => (
+                  <VerticalTimelineElement
+                    key={index}
+                    className="vertical-timeline-element--work"
+                    contentStyle={{ background: '#386FA5', color: '#fff' }}
+                    contentArrowStyle={{ borderRight: '7px solid  #386FA5' }}
+                    date={experience.experienceDate}
+                    iconStyle={{ background: '#386FA5', color: '#fff' }}
+                    icon={<Image
                       src={experience.image}
-                      className="w-[3rem] h-[3rem] object-cover rounded-full bg-transparent"
+                      className="w-full h-full p-2 object-fill rounded-full bg-white"
                       alt={experience.image}
                       width={100}
                       height={100}
                       quality={100}
-                    />
-                    <div className="flex flex-col items-start w-full">
+                    />}
+                  >
+                    <div className="flex flex-row items-center justify-between w-full">
+                      <h3 className="vertical-timeline-element-title font-bold text-[#F4BD83]">{experience.company}</h3>
                       <Link href={experience.link} target="_blank">
-                        <Text weight='bold' size='lg'>{experience.company}</Text>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                          <path strokeLinecap="round" strokeLinejoin="round" d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244" />
+                        </svg>
                       </Link>
-                      <Text weight='light' size='sm'>{experience.position}</Text>
                     </div>
-                  </div>
-                  <div className="flex flex-row items-center justify-start w-full md:w-auto ml-[4.2rem] md:ml-0">
-                    <Text size='sm' color={`${experience.isCurrent ? 'yellow' : 'gray'}`}>{experience.experienceDate}</Text>
-                  </div>
-                </div>
-              ))}
+                    <h4 className="vertical-timeline-element-subtitle font-light text-sm">{experience.position}</h4>
+                    <h6 className="mt-3 font-light text-sm">{experience.description}</h6>
+                  </VerticalTimelineElement>
+                ))}
+              </VerticalTimeline>
             </div>
           </div>
         </div>
