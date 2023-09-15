2d915f4906e0c8c940659e76aa3d8161768fc6e8
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2023-09-15 11:07:43 +0800

    feat: update experiences UI (#29)
    * chore: add scrollRestoration for nextjs
    
    * feat: add react-vertical-timeline-component
    
    * feat: update experiences UI

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    scrollRestoration: true,
+  },
   env: {
     DATABASE_URL: process.env.DATABASE_URL,
     SQUIDEX_API_URL: process.env.SQUIDEX_API_URL,
