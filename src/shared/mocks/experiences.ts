42ca5eac1c4cc909ceb823274324084b0fa1a2cb
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2024-02-22 18:52:11 +0800

    feat: add booky as new job experience

--- src/shared/mocks/experiences.ts
+++ src/shared/mocks/experiences.ts
@@ -1,29 +1,46 @@
 export const experiences = [
   {
-    image: 'https://firebasestorage.googleapis.com/v0/b/veoportfolio.appspot.com/o/44977563_1502404626526285_6391029430868770816_n.jpg?alt=media&token=6abef4e3-5415-4337-9178-96da70db20dd',
-    company: 'Halcyon Agile',
-    position: 'Software Developer',
-    description: 'Responsible for writing clean, efficient, and maintainable code for mobile applications, testing, and debugging mobile applications to ensure they meet quality standards and collaborating with other developers to share knowledge and best practices.',
-    link: 'https://halcyonagile.com.ph/',
-    experienceDate: 'June 2022 - Present',
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/veoportfolio.appspot.com/o/booky.png?alt=media&token=b8381cec-cbeb-414e-8100-6b7576ebde1e",
+    company: "Booky",
+    position: "Software Engineer",
+    description:
+      "This role involves planning and implementing features for mobile and web applications and server infrastructure, ensuring optimal user experience and adherence to development standards, while also tracking metrics, proposing improvements, and supporting internal tools as needed. The responsibilities span feature ideation, coding, testing, adherence to standards, metric tracking, improvement proposals, and internal tool support.",
+    link: "https://booky.ph/",
+    experienceDate: "February 2024 - Present",
     isCurrent: true,
   },
   {
-    image: 'https://firebasestorage.googleapis.com/v0/b/veoportfolio.appspot.com/o/spmi_icon.svg?alt=media&token=9a8bc5d0-2b5d-4dee-93ff-278e46248f6c',
-    company: 'Specialty Pulp Manufacturing, Inc.',
-    position: 'IT Specialist',
-    description: 'Manage software debugging, databases, LAN, creating company softwares, install and configure software and hardware, including printers, and other computer peripherals.',
-    link: 'https://www.spmi-pulp.com/',
-    experienceDate: 'February 2020 - June 2022',
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/veoportfolio.appspot.com/o/44977563_1502404626526285_6391029430868770816_n.jpg?alt=media&token=6abef4e3-5415-4337-9178-96da70db20dd",
+    company: "Halcyon Agile",
+    position: "Software Developer",
+    description:
+      "Responsible for writing clean, efficient, and maintainable code for mobile applications, testing, and debugging mobile applications to ensure they meet quality standards and collaborating with other developers to share knowledge and best practices.",
+    link: "https://halcyonagile.com.ph/",
+    experienceDate: "June 2022 - February 2024",
+    isCurrent: true,
+  },
+  {
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/veoportfolio.appspot.com/o/spmi_icon.svg?alt=media&token=9a8bc5d0-2b5d-4dee-93ff-278e46248f6c",
+    company: "Specialty Pulp Manufacturing, Inc.",
+    position: "IT Specialist",
+    description:
+      "Manage software debugging, databases, LAN, creating company softwares, install and configure software and hardware, including printers, and other computer peripherals.",
+    link: "https://www.spmi-pulp.com/",
+    experienceDate: "February 2020 - June 2022",
     isCurrent: false,
   },
   {
-    image: 'https://firebasestorage.googleapis.com/v0/b/veoportfolio.appspot.com/o/Puregold_logo%20(1).svg?alt=media&token=2096d215-71f3-4434-a2f5-176d7f9a8583',
-    company: 'Puregold Price Club, Inc.',
-    position: 'Graphic Artist',
-    description: 'Responsible for creating aesthetically promo posters and tarpaulins that accompany written text. Maintain store signages clean and presentable.',
-    link: 'https://puregold.com.ph/',
-    experienceDate: 'August 2019 - December 2019',
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/veoportfolio.appspot.com/o/Puregold_logo%20(1).svg?alt=media&token=2096d215-71f3-4434-a2f5-176d7f9a8583",
+    company: "Puregold Price Club, Inc.",
+    position: "Graphic Artist",
+    description:
+      "Responsible for creating aesthetically promo posters and tarpaulins that accompany written text. Maintain store signages clean and presentable.",
+    link: "https://puregold.com.ph/",
+    experienceDate: "August 2019 - December 2019",
     isCurrent: false,
   },
-]
+];
