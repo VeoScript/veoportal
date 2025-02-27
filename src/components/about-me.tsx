81a71e683e08fa16cd1f60d4a1ce6d6107d3a540
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2025-02-27 14:25:41 +0800

    feat: update about me component details (#70)

--- src/components/about-me.tsx
+++ src/components/about-me.tsx
@@ -13,14 +13,19 @@ const AboutMe = (): JSX.Element => {
         <ProfileHolder src="/images/jeromevillaruel_v2.webp" />
         <span className="absolute left-[58%] top-14 z-10 -rotate-12">
           <span className="flex animate-bounce rounded-full bg-default-white bg-opacity-80 p-3 text-center text-xs font-semibold dark:bg-default-dim-black dark:bg-opacity-80">
-            Jerome Villaruel 👋
+            Happy Coding! 👋
           </span>
         </span>
       </div>
       <div className="flex w-full flex-col items-center gap-y-2">
-        <h1 className="text-xl font-bold text-neutral-400">Software Developer</h1>
+        <h1 className="text-xl font-bold text-neutral-600 dark:text-neutral-400">
+          Software Developer
+        </h1>
+        <p className="text-center">
+          Hello! I am <span className="font-bold text-black dark:text-white">Jerome Villaruel</span>{" "}
+          also known as Veoscript
+        </p>
         <VoicePronounciation pronounceText={pronounceText} />
-        <p>Also known as Veoscript</p>
       </div>
       <h1 className="text-center text-[2rem] font-medium leading-10 tracking-tight md:text-[3rem] md:leading-[3.5rem]">
         Building <span className="font-bold text-blue-600">web</span>,{" "}
